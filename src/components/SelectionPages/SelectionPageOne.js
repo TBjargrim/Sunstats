2e46619ac4249d30c9995a4442f15edad1132eb1
tbjargrim <therese.bjargrim@student.KYH.se>
2021-03-19 11:13:07 +0100

    Added Components

---
+++ src/components/SelectionPages/SelectionPageOne.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SelectionPageOne() {
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default SelectionPageOne
+
---
+++ src/components/SelectionPages/SelectionPageTwo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SelectionPageTwo() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default SelectionPageTwo
