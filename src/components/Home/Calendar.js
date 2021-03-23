a6044d16ce020f1b8b7687cf6990e300b7e754bd
Jenny Magnessen <jennymagnessen@hotmail.com>
2021-03-23 14:22:37 +0100

    changed calendar type for months

--- src/components/Home/Calendar.js
+++ src/components/Home/Calendar.js
@@ -8,18 +8,47 @@ import { subDays, addDays } from "date-fns";
 import styled from "styled-components";
 import "./datepicker.css";
 
+const TravelBtn = styled.button`
+  display: block;
+  background: linear-gradient(
+    180deg
+    ,#F79521 0%,rgba(248,98,14,0) 100%),#F36565;
+  color: white;
+  border: none;
+  border-radius: 30px;
+  padding: 5px 40px 5px 40px;
+  font-size: 20px;
+  margin: 15px 0 10px;
+/*   box-shadow: 1px 2px 10px gray; */
+`;
+const BtnDiv = styled.div`
+display: flex;
+justify-content: center;
+`;
 
+
+function handleChange() {
+  let hej = document.getElementsByClassName("react-datepicker__input-container")
+ 
+  console.log(hej); // this will be a string value in datepicker input field
+}
 
 
 const Calendar = () => {
     const [startDate, setStartDate] = useState(new Date());
-    return (
+ 
+  return (
+      <>
       <DatePicker
         selected={startDate}
         onChange={date => setStartDate(date)}
         dateFormat="MM/yyyy"
         showMonthYearPicker
       />
+      <BtnDiv>
+        <TravelBtn type="submit" onClick={handleChange}>OK</TravelBtn>
+      </BtnDiv>
+      </>
     );
   };
 
--- src/components/Home/HomeStyled.js
+++ src/components/Home/HomeStyled.js
@@ -25,40 +25,24 @@ const FlexDiv = styled.div`
   }
 `;
 
-const TravelBtn = styled.button`
-  display: block;
-  background: linear-gradient(
-    180deg
-    ,#F79521 0%,rgba(248,98,14,0) 100%),#F36565;
-  color: white;
-  border: none;
-  border-radius: 30px;
-  padding: 5px 40px 5px 40px;
-  font-size: 20px;
-  margin: 15px 0 10px;
-/*   box-shadow: 1px 2px 10px gray; */
-`;
-const BtnDiv = styled.div`
-display: flex;
-justify-content: center;
-`;
+
+
 
 
 function HomeStyled() {
   return (
     <FlexDiv>
       <div>
+         {/*
         <h2>Vilken medeltemperatur vill du ha?</h2>
-       <Temp />
+       <Temp /> */}
       </div>
       <div>
         <h2>När vill du åka?</h2>
           <Calendar />
           <p><i>Startdatum för två veckors intervall</i></p>
       </div>
-      <BtnDiv>
-        <TravelBtn type="submit">OK</TravelBtn>
-      </BtnDiv>
+      
     </FlexDiv>
   );
 } 
