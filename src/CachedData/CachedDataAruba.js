9a31bd0640bb1251dc1065166e4a9c885de3dd0f
tbjargrim <therese.bjargrim@student.KYH.se>
2021-03-23 14:32:19 +0100

    average temp Aruba

---
+++ src/CachedData/CachedDataAruba.js
@@ -0,0 +1,225 @@
+export const Aruba2017Jan = {
+    "data": {
+        "request": [
+            {
+                "type": "City",
+                "query": "Oranjestad, Aruba"
+            }
+        ],
+        "weather": [
+            {
+                "date": "2017-01-07",
+                "astronomy": [
+                    {
+                        "sunrise": "07:04 AM",
+                        "sunset": "06:29 PM",
+                        "moonrise": "02:09 PM",
+                        "moonset": "02:00 AM",
+                        "moon_phase": "First Quarter",
+                        "moon_illumination": "63"
+                    }
+                ],
+                "maxtempC": "27",
+                "maxtempF": "81",
+                "mintempC": "27",
+                "mintempF": "80",
+                "avgtempC": "27",
+                "avgtempF": "80",
+                "totalSnow_cm": "0.0",
+                "sunHour": "10.1",
+                "uvIndex": "6",
+                "hourly": [
+                    {
+                        "time": "24",
+                        "tempC": "27",
+                        "tempF": "81",
+                        "windspeedMiles": "25",
+                        "windspeedKmph": "40",
+                        "winddirDegree": "91",
+                        "winddir16Point": "E",
+                        "weatherCode": "353",
+                        "weatherIconUrl": [
+                            {
+                                "value": "http://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0009_light_rain_showers.png"
+                            }
+                        ],
+                        "weatherDesc": [
+                            {
+                                "value": "Light rain shower"
+                            }
+                        ],
+                        "precipMM": "2.7",
+                        "precipInches": "0.1",
+                        "humidity": "82",
+                        "visibility": "10",
+                        "visibilityMiles": "6",
+                        "pressure": "1013",
+                        "pressureInches": "30",
+                        "cloudcover": "52",
+                        "HeatIndexC": "30",
+                        "HeatIndexF": "86",
+                        "DewPointC": "23",
+                        "DewPointF": "74",
+                        "WindChillC": "27",
+                        "WindChillF": "80",
+                        "WindGustMiles": "36",
+                        "WindGustKmph": "58",
+                        "FeelsLikeC": "30",
+                        "FeelsLikeF": "86",
+                        "uvIndex": "6"
+                    }
+                ]
+            }
+        ]
+    }
+};
+export const Aruba2018Jan = {
+    "data": {
+        "request": [
+            {
+                "type": "City",
+                "query": "Oranjestad, Aruba"
+            }
+        ],
+        "weather": [
+            {
+                "date": "2018-01-07",
+                "astronomy": [
+                    {
+                        "sunrise": "07:04 AM",
+                        "sunset": "06:29 PM",
+                        "moonrise": "No moonrise",
+                        "moonset": "11:47 AM",
+                        "moon_phase": "Waning Gibbous",
+                        "moon_illumination": "59"
+                    }
+                ],
+                "maxtempC": "28",
+                "maxtempF": "82",
+                "mintempC": "27",
+                "mintempF": "80",
+                "avgtempC": "27",
+                "avgtempF": "81",
+                "totalSnow_cm": "0.0",
+                "sunHour": "11.3",
+                "uvIndex": "7",
+                "hourly": [
+                    {
+                        "time": "24",
+                        "tempC": "28",
+                        "tempF": "82",
+                        "windspeedMiles": "19",
+                        "windspeedKmph": "30",
+                        "winddirDegree": "91",
+                        "winddir16Point": "E",
+                        "weatherCode": "116",
+                        "weatherIconUrl": [
+                            {
+                                "value": "http://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0002_sunny_intervals.png"
+                            }
+                        ],
+                        "weatherDesc": [
+                            {
+                                "value": "Partly cloudy"
+                            }
+                        ],
+                        "precipMM": "0.4",
+                        "precipInches": "0.0",
+                        "humidity": "80",
+                        "visibility": "10",
+                        "visibilityMiles": "6",
+                        "pressure": "1012",
+                        "pressureInches": "30",
+                        "cloudcover": "48",
+                        "HeatIndexC": "31",
+                        "HeatIndexF": "87",
+                        "DewPointC": "24",
+                        "DewPointF": "74",
+                        "WindChillC": "27",
+                        "WindChillF": "81",
+                        "WindGustMiles": "26",
+                        "WindGustKmph": "41",
+                        "FeelsLikeC": "31",
+                        "FeelsLikeF": "87",
+                        "uvIndex": "7"
+                    }
+                ]
+            }
+        ]
+    }
+}
+export const Aruba2019Jan = {
+    "data": {
+        "request": [
+            {
+                "type": "City",
+                "query": "Oranjestad, Aruba"
+            }
+        ],
+        "weather": [
+            {
+                "date": "2019-01-07",
+                "astronomy": [
+                    {
+                        "sunrise": "07:04 AM",
+                        "sunset": "06:29 PM",
+                        "moonrise": "08:12 AM",
+                        "moonset": "07:58 PM",
+                        "moon_phase": "Waxing Crescent",
+                        "moon_illumination": "3"
+                    }
+                ],
+                "maxtempC": "27",
+                "maxtempF": "80",
+                "mintempC": "26",
+                "mintempF": "78",
+                "avgtempC": "26",
+                "avgtempF": "79",
+                "totalSnow_cm": "0.0",
+                "sunHour": "11.3",
+                "uvIndex": "7",
+                "hourly": [
+                    {
+                        "time": "24",
+                        "tempC": "27",
+                        "tempF": "80",
+                        "windspeedMiles": "22",
+                        "windspeedKmph": "35",
+                        "winddirDegree": "91",
+                        "winddir16Point": "E",
+                        "weatherCode": "116",
+                        "weatherIconUrl": [
+                            {
+                                "value": "http://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0002_sunny_intervals.png"
+                            }
+                        ],
+                        "weatherDesc": [
+                            {
+                                "value": "Partly cloudy"
+                            }
+                        ],
+                        "precipMM": "0.4",
+                        "precipInches": "0.0",
+                        "humidity": "79",
+                        "visibility": "10",
+                        "visibilityMiles": "6",
+                        "pressure": "1014",
+                        "pressureInches": "30",
+                        "cloudcover": "46",
+                        "HeatIndexC": "29",
+                        "HeatIndexF": "84",
+                        "DewPointC": "22",
+                        "DewPointF": "72",
+                        "WindChillC": "26",
+                        "WindChillF": "79",
+                        "WindGustMiles": "31",
+                        "WindGustKmph": "50",
+                        "FeelsLikeC": "29",
+                        "FeelsLikeF": "84",
+                        "uvIndex": "7"
+                    }
+                ]
+            }
+        ]
+    }
+}
--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -3,12 +3,29 @@ import React from 'react';
 import { AuthUserContext, withAuthorization } from '../Session';
 import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
+import { Aruba2017Jan, Aruba2018Jan, Aruba2019Jan } from '../../CachedData/CachedDataAruba';
+
+const ArubaArr = [Aruba2017Jan, Aruba2018Jan, Aruba2019Jan];
+
+let resultArr = [];
+
+for (let i = 0; i < ArubaArr.length; i++) {
+  resultArr.push(parseInt(ArubaArr[i].data.weather[0].avgtempC));
+}
+
+let avgSum;
+for (var i = 0, sum = 0; i <= resultArr.length; sum += resultArr[i++]) {
+  avgSum = sum / resultArr.length
+};
+// console.log(avgSum.toFixed(1))
 
 const AccountPage = () => (
+
   <AuthUserContext.Consumer>
     {authUser => (
       <div>
         <h1>Account: {authUser.email}</h1>
+        <p>Medeltemperaturen för Aruba i januari är: {avgSum.toFixed(1)}</p>
         <PasswordForgetForm />
         <PasswordChangeForm />
       </div>
