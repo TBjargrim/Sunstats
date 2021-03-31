dfb349068a74165fc92a9db582eeb4ffcef65934
Jenny Magnessen <jennymagnessen@hotmail.com>
2021-03-31 14:42:01 +0200

    Add conditional rendering for wizard components

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,7 +3,7 @@ import "firebase/auth";
 import "firebase/database";
 
 const config = {
- 
+  
 };
 
 class Firebase {
--- src/components/Home/Temp.js
+++ src/components/Home/Temp.js
@@ -5,28 +5,20 @@ import { StyledText, StyledButtonWrapper, StyledButtons, StyledH2, StyledP, Styl
 import React, { useState } from 'react'
 import * as ROUTES from '../../constants/routes';
 
-const TravelSlc = styled.select`
-  font-size: 18px;
-  border-radius: 30px;
-  margin: 0 0 30px 0;
-  padding: 5px 60px 5px 60px;
-  font-size: 20px;
-  background: white;
-  border: none;
-  outline: none;
-  box-shadow: 1px 2px 10px gray;
-   background: url(http://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/br_down.png)
-    no-repeat right #fff;
-  -webkit-appearance: none;
-  background-position-x: 120px; 
-`;
+const Temp = ({ setTargetTemp, setTargetTempClick, setTargetTempClickBack }) => {
 
-const Temp = ({setTargetTemp}) => {
-
-   
   const handleClick = (e) => {
     const temp = parseInt(e.target.value);
+    console.log(temp);
     setTargetTemp(temp);
+  }
+  const handleNext = () => {
+    let clicked = true;
+    setTargetTempClick(clicked);
+  }
+  const handleBack = () => {
+    let clicked = false;
+    setTargetTempClickBack(clicked);
   }
       return(
     <div>
@@ -42,8 +34,8 @@ const Temp = ({setTargetTemp}) => {
     </StyledButtonWrapper>
     <StyledLinkSkip to={ROUTES.RESULT}>Hoppa över</StyledLinkSkip>
     <LinkWrapper>
-        <StyledLink to={ROUTES.WIZ}><FaArrowLeft /></StyledLink>
-        <StyledLink to={ROUTES.RESULT}><FaArrowRight /></StyledLink>
+        <StyledLink to={ROUTES.WIZ}><FaArrowLeft onClick={handleBack} /></StyledLink>
+       <StyledLink to={ROUTES.WIZ}> <FaArrowRight onClick={handleNext} /> </StyledLink> 
     </LinkWrapper>
   </div>
     );
