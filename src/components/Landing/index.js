91da6940c94b7963e108951ba5a080df536a0e10
Jenny Magnessen <jennymagnessen@hotmail.com>
2021-04-20 09:19:10 +0200

    Fix landing page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import BackgroundLanding from '../../Images/BackgroundLanding.jpg';
+/* import BackgroundLanding from '../../Images/BackgroundLanding.jpg'; */
+import BackgroundLanding from '../../Images/beach.jpg';
 import * as ROUTES from '../../constants/routes';
 import { Link } from 'react-router-dom'
 import Logo from '../../Images/sunstats_logo.png'
+
 
 
 const Wrapper = styled.div`
@@ -23,6 +25,44 @@ const ContainerDiv = styled.div`
   flex-direction:column;
   align-items:center;
   padding:150px;
+  p {
+    font-size: 33px;
+  }
+  div {
+    height: 10em;
+    position: relative;
+    margin-right: 0px;
+    width: 240px;
+  }
+  div > * {
+    visibility: hidden;
+    position: absolute;
+    top: 0;
+    left: 0;
+    animation: 20s autoplay1 infinite;
+  }
+  @keyframes autoplay1 {
+    0% {
+      visibility: visible;
+      opacity: 0%;
+    }
+    6% {
+      visibility: visible;
+      opacity: 25%;
+    }
+    20% {
+      visibility: visible;
+      opacity: 100%;
+    }
+    33.33% {
+      visibility: hidden;
+      opacity: 0%;
+    }
+  }
+  
+  div > *:nth-child(1) {animation-delay: 0s}
+  div > *:nth-child(2) {animation-delay: 6s}
+  div > *:nth-child(3) {animation-delay: 12s}
 `
 const SignInBtn = styled(Link)`
   width:330px;
@@ -93,13 +133,11 @@ top:50px;
 width:250px;
    animation-name: moveInleft;
     animation-duration: 3s;
-
 @keyframes moveInleft {
     0% {
         opacity: 0;
         transform: translateX(-100px);
     }
-
     80% {
         transform: translateX(10px);
     }
@@ -126,6 +164,13 @@ const Landing = () => (
     </li> */}
       <SignInBtn to={ROUTES.SIGN_IN}>Logga in</SignInBtn>
       <CreAccBtn to={ROUTES.SIGN_UP}> Skapa konto</CreAccBtn>
+
+      <div>
+        <p>Hitta och planera dina resmål</p>
+        <p>Sök på temperaturer, datum och destinationer</p>
+      <p>Bli klimatsmart med våra funktioner</p>
+      </div>
+
     </ContainerDiv>
   </Wrapper>
 
